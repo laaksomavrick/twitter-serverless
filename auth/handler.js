51994c3ff5c480fc01e67e25de8521f2aaf597f1
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-04-13 18:12:30 -0400

    tinkering still; auth will be replaced by cognito

--- auth/handler.js
+++ auth/handler.js
@@ -1,11 +1,42 @@
 "use strict";
 
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+// will have to write some promise / PascalCase wrapping stuff over the dynamodb client
+
 module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event,
-    }),
-  };
+  try {
+    const putParams = {
+      TableName: "auth",
+      Item: {
+        auth_id: "12345",
+        email: "blah",
+        password: "qwe",
+      },
+    };
+
+    const getParams = {
+      TableName: "auth",
+      Key: {
+        auth_id: "12345",
+      },
+    };
+
+    await dynamo.put(putParams).promise();
+
+    const { Item } = await dynamo.get(getParams).promise();
+
+    return {
+      statusCode: 200,
+      body: Item,
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: e.toString(),
+      }),
+    };
+  }
 };
